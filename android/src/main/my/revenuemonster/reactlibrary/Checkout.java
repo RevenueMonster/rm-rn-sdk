3e66fbbeef6b4782f483d8c9e5042f6f2d4fd60c
Gan Jun Kai <kuhn96@gmail.com>
2019-07-05 19:18:06 +0800

    Add rm-android

---
+++ android/src/main/my/revenuemonster/reactlibrary/Checkout.java
@@ -0,0 +1,14 @@
+package my.revenuemonster.reactlibrary;
+
+import com.revenuemonster.payment.Checkout;
+import com.revenuemonster.payment.PaymentResult;
+import com.revenuemonster.payment.constant.Env;
+import com.revenuemonster.payment.constant.Method;
+import com.revenuemonster.payment.model.Error;
+import com.revenuemonster.payment.model.Transaction;
+
+class CheckoutActivity {
+    public newCheckoutActivity() {
+        new Checkout(getApplciation()).
+    }
+}
