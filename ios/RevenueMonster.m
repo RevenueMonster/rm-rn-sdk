3f5ed9e654318c05367ec19a793c33a498422364
Gan Jun Kai <kuhn96@gmail.com>
2019-07-18 14:31:20 +0800

    fix(ios): remove unused codes

--- ios/RevenueMonster.m
+++ ios/RevenueMonster.m
@@ -1,13 +1,28 @@
+// #import "RevenueMonster.h"
+
+// @implementation RevenueMonster
+
+// RCT_EXPORT_MODULE()
+
+// RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+// {
+//     // TODO: Implement some real useful functionality
+//     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+// }
+
+// @end
+
+
 #import "RevenueMonster.h"
+#import <React/RCTLog.h>
 
 @implementation RevenueMonster
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
 {
-    // TODO: Implement some real useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
 @end
